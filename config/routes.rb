83e8edd2974a70f07335feb94724ec565405ad67
gabo_71096 <gabo_71096@hotmail.com>
2021-12-09 22:00:11 -0300

    modified root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :noticia
-  root 'news#index'
+  root 'noticia#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
