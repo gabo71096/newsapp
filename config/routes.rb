7ae125e11d12a9306e89676735eb6132e6146d6a
gabo_71096 <gabo_71096@hotmail.com>
2021-12-12 17:14:10 -0300

    added active admin (and devise) routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   get '/auth/:provider/callback', to: 'sessions#create'
