35e7fdbe5a06b25c849f42b6b8689875a20134d7
gabo_71096 <gabo_71096@hotmail.com>
2021-12-12 14:01:04 -0300

    added authorizations and users tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_013531) do
+ActiveRecord::Schema.define(version: 2021_12_12_161936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authorizations", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "noticia", force: :cascade do |t|
     t.string "title"
@@ -21,6 +29,13 @@ ActiveRecord::Schema.define(version: 2021_12_10_013531) do
     t.string "author"
     t.text "body"
     t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
