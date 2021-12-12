278dd677fe94e4f124f7e92760641425af429163
gabo_71096 <gabo_71096@hotmail.com>
2021-12-12 16:04:04 -0300

    removed post callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
-  post '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#failure'
   resources :noticia
