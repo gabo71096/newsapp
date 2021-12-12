f78545957e86728d6c9803f7995f9ca5567fa0c9
gabo_71096 <gabo_71096@hotmail.com>
2021-12-12 11:07:58 -0300

    jquery for reload page when select changes

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,15 @@
+$("#author").change(function () {
+  if (window.location.href.includes("?")) {
+    window.location = window.location.href + "&author=" + $(this).val();
+  } else {
+    window.location = window.location.href + "?author=" + $(this).val();
+  }
+});
+
+$("#order").change(function () {
+  if (window.location.href.includes("?")) {
+    window.location = window.location.href + "&order=" + $(this).val();
+  } else {
+    window.location = window.location.href + "?order=" + $(this).val();
+  }
+});
