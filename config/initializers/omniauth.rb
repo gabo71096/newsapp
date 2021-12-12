538ae73888995193f107afeba185eb7759f96511
gabo_71096 <gabo_71096@hotmail.com>
2021-12-12 14:41:27 -0300

    silence get warning

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], skip_jwt: true
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
 end
+OmniAuth.config.allowed_request_methods = %i[get]
+OmniAuth.config.silence_get_warning = true
