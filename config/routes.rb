845d06d34e7667b70c7439ed179e8a5034185694
gabo_71096 <gabo_71096@hotmail.com>
2021-12-12 14:00:40 -0300

    added auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new', as: :login
+  get '/logout', to: 'sessions#destroy'
+  match '/auth/:provider/callback', to: 'sessions#create', via: %i[get post]
+  get '/auth/failure', to: 'sessions#failure'
   resources :noticia
   root 'noticia#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
