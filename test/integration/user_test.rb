8fe9ce8f3ef4c2444a93a1a5b0d7587bdddea181
gabo_71096 <gabo_71096@hotmail.com>
2021-12-14 10:34:44 -0300

    added comments, changed test name

--- test/integration/user_test.rb
+++ test/integration/user_test.rb
@@ -1,13 +1,14 @@
 # Personal integration test file for job application at GenoSUR
 require '../test_helper'
+require '../application_system_test_case'
 
-class UserTest < ActionDispatch::IntegrationTest
+class ActiveAdminTest < ActionDispatch::IntegrationTest
   def setup
     @new = noticia :one
     @user = admin_users :one
   end
 
-  test 'login and post news' do
+  test 'login active admin and post news' do
     get '/admin/login'
     assert_response :success
     post '/admin/login', params: { session: { email: @user.email, password: @user.encrypted_password } }
@@ -24,3 +25,20 @@ class UserTest < ActionDispatch::IntegrationTest
     assert_response :found
   end
 end
+
+# For some reason I can't login, credentials are in db:seeds in test environment,
+# so I should get the posibility to login, but doesn't work
+# class NewsTest < ApplicationSystemTestCase
+#   def setup
+#     @admin_users = admin_users :one
+#     @noticia = noticia :one
+#   end
+
+#   test 'visit index and check content' do
+#     visit new_admin_user_session_url
+#     fill_in 'Email', with: @admin_users.email
+#     fill_in 'Password', with: @admin_users.encrypted_password
+#     click_on 'Login'
+#     click_on 'Noticia'
+#   end
+# end
