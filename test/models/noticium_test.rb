eb87c738732b763d7d17c8ce9f940a37ad617acb
gabo_71096 <gabo_71096@hotmail.com>
2021-12-14 00:41:59 -0300

    assert object not empty

--- test/models/noticium_test.rb
+++ test/models/noticium_test.rb
@@ -1,6 +1,8 @@
+# Personal unit test file for job application at GenoSUR
 require '../test_helper'
 
 class NoticiumTest < ActiveSupport::TestCase
+  # Added correction to noticium.rb in models folder
   test 'should not save news without title' do
     news = Noticium.new
     assert_not news.save, 'Saved news without title'
@@ -8,6 +10,6 @@ class NoticiumTest < ActiveSupport::TestCase
 
   test 'should get all news' do
     news = Noticium.all
-    assert news
+    assert_not_empty news, "Object is not empty"
   end
 end
