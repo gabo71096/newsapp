623251dc5c1017a2c445298547caecf850834f47
gabo_71096 <gabo_71096@hotmail.com>
2021-12-14 10:46:26 -0300

    added controller test

--- test/models/admin_user_test.rb
+++ test/models/admin_user_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# Personal unit test file for job application at GenoSUR
+require "../test_helper"
 
-class AdminUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class AdminUserTest < ActionDispatch::IntegrationTest
+  test 'get index route' do
+    get '/admin/login'
+    assert_response :success
+  end
 end
